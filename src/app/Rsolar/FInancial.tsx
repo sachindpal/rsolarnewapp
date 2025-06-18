52966d8124d6a921ff93aea3a75dc393f8ae5ab1
Sachin Pal <sachinpal@farmakart.com>
2025-06-18 16:32:54 +0530

    working on the filters for graph

--- src/app/Rsolar/FInancial.tsx
+++ src/app/Rsolar/FInancial.tsx
@@ -123,69 +123,122 @@ const Financial = ({color,selectedValue}:any) => {
   }
 
 
-  const homeData = data.map((d:any) => ({
+//   const homeData = data.map((d:any) => ({
+//     x: d.hour,
+//     y: d.home,
+//     label: `Home: ${Math.round((d.home / (d.home + d.grid)) * 100)}%`,
+//   }));
+
+//   const gridData = data.map((d:any) => ({
+//     x: d.hour,
+//     y: d.grid,
+//     label: `Grid: ${Math.round((d.grid / (d.home + d.grid)) * 100)}%`,
+//   }));
+
+//   return (
+//     <View style={styles.container}>
+//       {/* Sticky Y-axis */}
+//       {/* <View style={styles.yAxisContainer}>
+//         <VictoryChart
+//           height={300}
+//           width={70} // Narrow chart just for Y-axis
+//           padding={{ left: 60, top: 20, bottom: 50, right: 0 }}
+//           domainPadding={{ y: 10 }}
+//         >
+//           <VictoryAxis dependentAxis />
+//         </VictoryChart>
+//       </View> */}
+
+//       {/* Scrollable chart */}
+//       <ScrollView
+//         horizontal
+//         pagingEnabled
+//         showsHorizontalScrollIndicator={false}
+//       >
+//         <VictoryChart
+//           height={300}
+//           width={chartWidth}
+//           padding={{ left: 10, top: 20, bottom: 50, right: 20 }}
+//           domainPadding={{ x: 20 }}
+//         >
+//           <VictoryAxis style={{
+//       tickLabels: { fill: color.labelgrey, fontSize: 10 }, // X-axis label color
+//     }} />
+//           <VictoryStack>
+//             <VictoryBar
+//               data={homeData}
+//               style={{ data: { fill: 'orange', fillOpacity: 0.5 } }}
+//               labelComponent={<VictoryTooltip renderInPortal={false}/>}
+//               barWidth={barWidth * 0.3}
+              
+//             />
+//             <VictoryBar
+//               data={gridData}
+//               style={{ data: { fill: 'orange', fillOpacity: 1 } }}
+//               labelComponent={<VictoryTooltip renderInPortal={false}/>}
+//               barWidth={barWidth * 0.3}
+//               cornerRadius={{ top: 10, bottom: 0 }}
+//               labels={({ datum }) => `${datum.y}`}  // show y-value
+//             />
+//           </VictoryStack>
+//         </VictoryChart>
+//       </ScrollView>
+//     </View>
+//   );
+// };
+const homeData = data.map((d: any) => ({
     x: d.hour,
     y: d.home,
-    label: `Home: ${Math.round((d.home / (d.home + d.grid)) * 100)}%`,
+    // label: `₹${d.home}`,
   }));
-
-  const gridData = data.map((d:any) => ({
+  
+  const gridData = data.map((d: any) => ({
     x: d.hour,
     y: d.grid,
-    label: `Grid: ${Math.round((d.grid / (d.home + d.grid)) * 100)}%`,
+    label: `₹${parseFloat(d.grid).toFixed(1)}`,
   }));
-
+  
   return (
     <View style={styles.container}>
-      {/* Sticky Y-axis */}
-      {/* <View style={styles.yAxisContainer}>
-        <VictoryChart
-          height={300}
-          width={70} // Narrow chart just for Y-axis
-          padding={{ left: 60, top: 20, bottom: 50, right: 0 }}
-          domainPadding={{ y: 10 }}
-        >
-          <VictoryAxis dependentAxis />
-        </VictoryChart>
-      </View> */}
-
-      {/* Scrollable chart */}
       <ScrollView
         horizontal
         pagingEnabled
         showsHorizontalScrollIndicator={false}
       >
         <VictoryChart
-          height={300}
+        //   height={400}
           width={chartWidth}
-          padding={{ left: 10, top: 20, bottom: 50, right: 20 }}
+          padding={{ left: 10, top: 40, bottom: 50, right: 20 }}
           domainPadding={{ x: 20 }}
         >
-          <VictoryAxis style={{
-      tickLabels: { fill: color.labelgrey, fontSize: 10 }, // X-axis label color
-    }} />
+          <VictoryAxis
+            style={{
+              tickLabels: { fill: color.labelgrey, fontSize: 10 },
+            }}
+          />
           <VictoryStack>
             <VictoryBar
               data={homeData}
               style={{ data: { fill: 'orange', fillOpacity: 0.5 } }}
-              labelComponent={<VictoryTooltip renderInPortal={false}/>}
+            //   labels={({ datum }) => `₹${parseFloat(datum.y).toFixed(1)}`}
+              labelComponent={<VictoryNative.VictoryLabel dy={-10} />}
               barWidth={barWidth * 0.3}
-              
             />
             <VictoryBar
               data={gridData}
               style={{ data: { fill: 'orange', fillOpacity: 1 } }}
-              labelComponent={<VictoryTooltip renderInPortal={false}/>}
+            //   labels={({ datum }) => `₹${parseFloat(datum.y).toFixed(1)}`}
+              labelComponent={<VictoryNative.VictoryLabel dy={-10} />}
               barWidth={barWidth * 0.3}
               cornerRadius={{ top: 10, bottom: 0 }}
-              labels={({ datum }) => `${datum.y}`}  // show y-value
             />
           </VictoryStack>
         </VictoryChart>
       </ScrollView>
     </View>
   );
-};
+  
+}
 
 const styles = StyleSheet.create({
   container: {
